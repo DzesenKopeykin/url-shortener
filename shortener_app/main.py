e3dea7960c7bab392165ef11d49e6197335d7c5f
Dzenis Kapeikin <kopeykindzzesen@gmail.com>
2022-06-07 14:01:08 +0300

    added /admin/{secret_key} endpoint

--- shortener_app/main.py
+++ shortener_app/main.py
@@ -1,9 +1,11 @@
 from fastapi import Depends, FastAPI, HTTPException, Request
 from fastapi.responses import RedirectResponse
 from sqlalchemy.orm import Session
+from starlette.datastructures import URL
 from validators import url as validate_url  # type: ignore
 
 from shortener_app import crud, models, schemas
+from shortener_app.config import get_settings
 from shortener_app.database import SessionLocal, engine
 
 app = FastAPI()
@@ -27,21 +29,28 @@ def raise_not_found(request: Request) -> None:
     raise HTTPException(status_code=404, detail=message)
 
 
+def get_admin_info(db_url: models.URL) -> schemas.URLInfo:
+    base_url = URL(get_settings().base_url)
+    admin_endpoint = app.url_path_for(
+        "administration info", secret_key=db_url.secret_key
+    )
+    db_url.url = str(base_url.replace(path=db_url.key))
+    db_url.admin_url = str(base_url.replace(path=admin_endpoint))
+    return db_url
+
+
 @app.get("/")
 def read_root():
     return "Welcome to the URL shortener API :)"
 
 
 @app.post("/url", response_model=schemas.URLInfo)
-def create_url(url: schemas.URLBase, db: Session = Depends(get_db)) -> models.URL:
+def create_url(url: schemas.URLBase, db: Session = Depends(get_db)):
     if not validate_url(url.target_url):
         raise_bad_request(message="Your provided URL is not valid")
 
     db_url = crud.create_db_url(db, url)
-    db_url.url = db_url.key
-    db_url.admin_url = db_url.secret_key
-
-    return db_url
+    return get_admin_info(db_url)
 
 
 @app.get("/{url_key}")
@@ -50,5 +59,15 @@ def forward_to_target_url(
 ):
     if db_url := crud.get_db_url_by_key(db, url_key):
         return RedirectResponse(db_url.target_url)
+    else:
+        raise_not_found(request)
+
+
+@app.get(
+    "/admin/{secret_key}", name="administration info", response_model=schemas.URLInfo
+)
+def get_url_info(secret_key: str, request: Request, db: Session = Depends(get_db)):
+    if db_url := crud.get_db_url_by_secret_key(db, secret_key):
+        return get_admin_info(db_url)
     else:
         raise_not_found(request)
