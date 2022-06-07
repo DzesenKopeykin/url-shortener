085762f616953f701f390284e538223b14b70ce9
Dzenis Kapeikin <kopeykindzzesen@gmail.com>
2022-06-07 16:33:14 +0300

    added endpoint /admin/{secret_key} to deactivate urls

--- shortener_app/crud.py
+++ shortener_app/crud.py
@@ -32,3 +32,12 @@ def increment_clicks_for_db_url(db: Session, db_url: models.URL) -> models.URL:
     db.commit()
     db.refresh(db_url)
     return db_url
+
+
+def deactivate_db_url_by_secret_key(db: Session, secret_key: str) -> models.URL | None:
+    db_url = get_db_url_by_secret_key(db, secret_key)
+    if db_url:
+        db_url.is_active = False
+        db.commit()
+        db.refresh(db_url)
+    return db_url
